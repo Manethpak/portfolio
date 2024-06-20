1a8c2c5d9f1e04e25911aa4ad54a89a9cd3890c5
manethpak <manethpak00@gmail.com>
2024-06-20 15:51:46 +0700

    feat: timezone, contact and social links

---
+++ src/components/section/index.ts
@@ -0,0 +1,4 @@
+export { default as IntroSection } from "./Intro.astro";
+export { default as TimezoneSection } from "./Timezone.astro";
+export { default as ContactSection } from "./Contact.astro";
+export { default as SocialSection } from "./Social.astro";
