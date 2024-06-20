411bce5719ead2d9a82dacd7447268ae947c3876
manethpak <manethpak00@gmail.com>
2024-06-20 17:21:16 +0700

    chore: move social links

--- src/components/section/index.ts
+++ src/components/section/index.ts
@@ -1,4 +1,3 @@
 export { default as IntroSection } from "./Intro.astro";
 export { default as TimezoneSection } from "./Timezone.astro";
 export { default as ContactSection } from "./Contact.astro";
-export { default as SocialSection } from "./Social.astro";
