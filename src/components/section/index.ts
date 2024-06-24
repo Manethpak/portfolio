e6a487f576278fded8121158da797cb9b769e173
manethpak <manethpak00@gmail.com>
2024-06-24 23:41:17 +0700

    partial commit

--- src/components/section/index.ts
+++ src/components/section/index.ts
@@ -1,3 +1,6 @@
 export { default as IntroSection } from "./Intro.astro";
 export { default as TimezoneSection } from "./Timezone.astro";
 export { default as ContactSection } from "./Contact.astro";
+export { default as AboutMeSection } from "./AboutMe.astro";
+export { default as ExperienceSection } from "./Experience.astro";
+export { default as CreditSection } from "./Credit.astro";
