31afd7ca7ff93f8832022699a4c7b564718d065f
manethpak <manethpak00@gmail.com>
2024-07-10 22:19:44 +0700

    chore: remove not ready feature

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
 
 const svgToDataUri = require("mini-svg-data-uri");
 
-const colors = require("tailwindcss/colors");
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
