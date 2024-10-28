160770440a56bdd593017288ae8647eda2349107
manethpak <manethpak00@gmail.com>
2024-10-28 16:26:44 +0700

    chore: change to netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig, passthroughImageService } from "astro/config";
-
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "hybrid",
-  adapter: vercel(),
+  adapter: netlify(),
   image: {
     service: passthroughImageService(),
   },
