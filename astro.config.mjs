a386208e052870a1e711e366eb373ce2616992e5
manethpak <manethpak00@gmail.com>
2024-11-08 11:13:29 +0700

    Revert "chore: change to netlify config"
    This reverts commit 160770440a56bdd593017288ae8647eda2349107.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig, passthroughImageService } from "astro/config";
+
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "hybrid",
-  adapter: netlify(),
+  adapter: vercel(),
   image: {
     service: passthroughImageService(),
   },
