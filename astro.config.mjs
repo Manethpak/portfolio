5583f04fb2524ec2ab66b6ff692c8855f7f318dd
manethpak <manethpak00@gmail.com>
2025-02-17 22:32:26 +0700

    chore: remove react and config vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,25 @@
 import { defineConfig, passthroughImageService } from "astro/config";
 
-import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [tailwind()],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    isr: {
+      expiration: 600,
+    },
+  }),
   image: {
     service: passthroughImageService(),
+  },
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "viewport",
   },
 });
